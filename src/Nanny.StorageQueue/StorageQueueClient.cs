aad209104dbed6a50fabf92a50fc6376d12dd96c
Vinícius Batista de Souza <visouza@microsoft.com>
2018-05-03 08:11:09 -0300

    Adding support for azure storage and fixing some problems related to the usage of resources from the cluster

---
+++ src/Nanny.StorageQueue/StorageQueueClient.cs
@@ -0,0 +1,14 @@
+﻿using Nanny.Common.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Nanny.StorageQueue
+{
+    public class StorageQueueClient : IQueueClient
+    {
+        public Task<long> GetMessageCountAsync(string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
