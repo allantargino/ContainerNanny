22bb02c31903d6aa04349335357aaa296770b920
Allan Targino <allan_targino@hotmail.com>
2018-05-26 12:13:47 -0300

    installing .net core before build. got scripts from kubernetes-client/c# repo

---
+++ install-linux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo 'Installing .NET Core...'
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get -qq update
+sudo apt-get install -y dotnet-sdk-2.1.300-rc1-008673
