2491ff3b033027dadc140086268d9f0470cbdd13
Allan Targino <allan_targino@hotmail.com>
2018-05-26 15:36:22 -0300

    Update buildandpush.sh

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit on any error
+set -e
+
 if [ $# -lt 2 ]
   then
     echo "provide azure register username and password"
@@ -9,36 +12,11 @@ fi
 echo 'Building Dotnet project'
 dotnet restore ./Queue.sln && dotnet publish ./Queue.sln -c Release -o ./obj/Docker/publish
 
-if [ $? -lt 0 ]
-then
-    echo 'Error building the project'
-    exit -1
-fi
-
-
 echo 'Creating docker image'
 docker-compose build
-
-if [ $? -lt 0 ]
-then
-    echo 'Error creating image'
-    exit -1
-fi
 
 echo 'docker login'
 docker login visouza.azurecr.io -u $1 -p $2
 
-if [ $? -lt 0 ]
-then
-    echo 'Error Docker Login'
-    exit -1
-fi
-
 echo 'docker push image to azure visouza.azurecr.io/app/queueconsumer'
 docker push visouza.azurecr.io/app/queueconsumer
-
-if [ $? -lt 0 ]
-then
-    echo 'Error Docker Push'
-    exit -1
-fi
