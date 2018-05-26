b4a6a1576f8dd16a8d1b0f906d8a5e002bff2cbe
Allan Targino <allan_targino@hotmail.com>
2018-05-26 11:56:21 -0300

    fix #18

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+# Ensure no compile errors in all projects
+find . -name *.csproj -exec dotnet build {} \;
+
+# Execute Unit tests
+# cd tests/KubernetesClient.Tests
+# dotnet restore
+# dotnet test
